85b6a0b9879af249ddcd0f986e16f52462402060
Johnathan <ohkowawa@gmail.com>
2025-12-07 20:13:32 +0000

    finished Day 7 Part 1

---
+++ day-7/part-one.hs
@@ -0,0 +1,36 @@
+import Data.Set as Set
+
+-- Split one line of the beam and also count how many splits occurred
+splitBeam :: Set Int -> String -> (Set Int, Int)
+splitBeam curLst inp
+    | Set.null curLst = (initial, 0)
+    | otherwise       = (final, splits)
+  where
+    initial = posInp inp (0, empty)
+    equal = intersection curLst initial
+    lower  = Set.map (\x -> x - 1) equal
+    higher = Set.map (\x -> x + 1) equal
+    final = difference (lower `union` higher `union` curLst) equal
+    splits = Set.size equal
+
+posInp :: String -> (Int, Set Int) -> Set Int
+posInp [] (_, ret) = ret
+posInp (x:xs) (index, confirmed)
+    | x == '^'  = posInp xs (index + 1, insert index confirmed)
+    | otherwise = posInp xs (index + 1, confirmed)
+
+combinedFunc :: (Set Int, Int) -> [String] -> (Set Int, Int)
+combinedFunc acc [] = acc
+combinedFunc (curSet, curSplits) (x:xs) =
+    combinedFunc (nextSet, curSplits + addedSplits) xs
+  where
+    (nextSet, addedSplits) = splitBeam curSet x
+
+
+main :: IO()
+main = do
+    file <- readFile "./input.txt"
+    let ls = lines file
+    let startSet = posInp (head ls) (0, empty)
+    let (_, totalSplits) = combinedFunc (startSet, 0) ls
+    print totalSplits
